5b8aed13adc5da9bdd20ca49088fc67ef0d3660c
Elia Dotor Puente <elia.dotor@gmail.com>
2022-01-28 13:44:24 +0100

    Initial Commit

---
+++ HarryPotterCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterCharacters
+//
+//  Created by Elia Dotor Puente on 28/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
