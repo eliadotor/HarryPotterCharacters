e48943e16ce46ac23d1bfee4db0f00f5b5f6ff88
Elia Dotor Puente <elia.dotor@gmail.com>
2022-02-06 13:32:04 +0100

    Houses Colors

--- HarryPotterCharacters/Modules/Detail/UI/DetailViewController.swift
+++ HarryPotterCharacters/Modules/Detail/UI/DetailViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import Kingfisher
 
-struct WandViewModel {
-    let wood: String
-    let core: String
+enum Houses: String {
+    case Gryffindor = "Gryffindor"
+    case Slytherin = "Slytherin"
+    case Hufflepuff = "Hufflepuff"
+    case Ravenclaw = "Ravenclaw"
 }
 
 struct DetailViewModel {
@@ -44,11 +46,23 @@ class DetailViewController: UIViewController {
 
 
 extension DetailViewController: DetailViewContract {
+    
     func configure(with viewModel: DetailViewModel) {
+        var color : UIColor {
+            switch viewModel.house {
+            case Houses.Gryffindor.rawValue: return UIColor(named: "Gryffindor") ?? UIColor.systemRed
+            case Houses.Slytherin.rawValue: return UIColor(named: "Slytherin") ?? UIColor.systemGreen
+            case Houses.Hufflepuff.rawValue: return UIColor(named: "Hufflepuff") ?? UIColor.systemYellow
+            case Houses.Ravenclaw.rawValue: return UIColor(named: "Ravenclaw") ?? UIColor.systemBlue
+            default:
+                return UIColor.black
+            }
+        }
         DispatchQueue.main.async { [self] in
             self.detailImage.kf.setImage(with: viewModel.imageUrl, placeholder: UIImage(named: viewModel.house))
             nameCharacter.text = "\(viewModel.name) - "
             labelHouse.text = viewModel.house
+            labelHouse.textColor = color
             nameLabelActor.text = "Actor: "
             nameActor.text = viewModel.actor
             patronusLabel.text = "Patronus: "
